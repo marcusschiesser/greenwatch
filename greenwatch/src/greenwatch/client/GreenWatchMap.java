e7de6d38a1e45fd42a13d25801b94874e287c040
wernerw.maier@gmail.com <wernerw.maier@gmail.com@24e5345e-f19f-c75b-200d-78b3974f6bd8>
2011-04-16 12:08:04 +0000

    Checkout test

--- greenwatch/src/greenwatch/client/GreenWatchMap.java
+++ greenwatch/src/greenwatch/client/GreenWatchMap.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class GreenWatchMap extends Activity {
     /** Called when the activity is first created. */
+	// Dieser Kommentar ist sinnfrei
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
