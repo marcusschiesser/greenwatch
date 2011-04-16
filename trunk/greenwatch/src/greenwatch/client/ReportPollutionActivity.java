b0e68253069bc67e520574fbe12fa578760d9c41
jkabrede@googlemail.com <jkabrede@googlemail.com@24e5345e-f19f-c75b-200d-78b3974f6bd8>
2011-04-16 13:10:43 +0000

    new Name

---
+++ trunk/greenwatch/src/greenwatch/client/ReportPollutionActivity.java
@@ -0,0 +1,13 @@
+package greenwatch.client;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ReportPollutionActivity  extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.report);
+       
+    }
+
+}
