e0b2ff0a63655bec13022972f3304ec26d46bd66
wernerw.maier@gmail.com <wernerw.maier@gmail.com@24e5345e-f19f-c75b-200d-78b3974f6bd8>
2011-04-16 16:13:59 +0000

    Drawing POI on the map

--- greenwatch/src/greenwatch/client/GreenWatchMap.java
+++ greenwatch/src/greenwatch/client/GreenWatchMap.java
@@ -9,11 +9,15 @@ import java.util.List;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
 
 public class GreenWatchMap extends MapActivity {
     private ProgressDialog mProgressDialog;
@@ -32,7 +36,22 @@ public class GreenWatchMap extends MapActivity {
 		mProgressDialog.setMessage(getResources().getString(R.string.edit_waiting));
 		mProgressDialog.setCancelable(false);
 		mProgressDialog.setIndeterminate(true);
+		
+		// draw POI on the map
+		List<Overlay> mapOverlays = mapView.getOverlays();
+		Drawable drawable = this.getResources().getDrawable(R.drawable.atom);
+		GreenWatchItemizedOverlay itemizedoverlay = new GreenWatchItemizedOverlay(drawable);
+		
+		// Test point
+		GeoPoint point = new GeoPoint(19240000,-99120000);
+		OverlayItem overlayitem = new OverlayItem(point, "Hola, Mundo!", "I'm in Mexico City!");
         
+		GeoPoint point2 = new GeoPoint(35410000, 139460000);
+		OverlayItem overlayitem2 = new OverlayItem(point2, "Sekai, konichiwa!", "I'm in Japan!");
+		
+		itemizedoverlay.addOverlay(overlayitem);
+		itemizedoverlay.addOverlay(overlayitem2);
+		mapOverlays.add(itemizedoverlay);
     }
     
     @Override
