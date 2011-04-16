d46db846c2763467531d58978de292fec3f310ad
Marcus.Schiesser@googlemail.com <Marcus.Schiesser@googlemail.com@24e5345e-f19f-c75b-200d-78b3974f6bd8>
2011-04-16 13:43:11 +0000

    - added example service call

--- trunk/greenwatch/src/greenwatch/client/GreenWatchMap.java
+++ trunk/greenwatch/src/greenwatch/client/GreenWatchMap.java
@@ -2,18 +2,32 @@ package greenwatch.client;
 
 
 
+import greenwatch.client.service.GetPollutionService;
+import greenwatch.common.vo.PollutionVO;
+
+import java.util.List;
+
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 public class GreenWatchMap extends Activity {
-    /** Called when the activity is first created. */
+    private ProgressDialog mProgressDialog;
+
+	/** Called when the activity is first created. */
 	// Dieser Kommentar ist sinnfrei
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+		mProgressDialog = new ProgressDialog(this);
+		mProgressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+		mProgressDialog.setMessage(getResources().getString(R.string.edit_waiting));
+		mProgressDialog.setCancelable(false);
+		mProgressDialog.setIndeterminate(true);
         
     }
     
@@ -24,6 +38,22 @@ public class GreenWatchMap extends Activity {
 					ReportPollutionActivity.class);
 			startActivity(intentRep);
 		}
+	}
+	
+	public void getPolutions(long lat, long lng) {
+		GetPollutionService service = new GetPollutionService() {
+    		@Override
+    		protected void onPreExecute() {
+    			mProgressDialog.show();
+    		}
+			@Override
+			protected void onPostExecute(List<PollutionVO> result) {
+				mProgressDialog.dismiss();
+				// TODO do something with the pollution!!!
+			}
+    	};
+    	
+		service.execute(lat, lng);		
 	}
     
     
