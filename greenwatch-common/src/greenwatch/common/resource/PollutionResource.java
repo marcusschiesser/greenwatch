824888dc0b095d0bfb1aa2a02c449743ab5c7019
Marcus.Schiesser@googlemail.com <Marcus.Schiesser@googlemail.com@24e5345e-f19f-c75b-200d-78b3974f6bd8>
2011-04-16 16:11:09 +0000

    changed to array

--- greenwatch-common/src/greenwatch/common/resource/PollutionResource.java
+++ greenwatch-common/src/greenwatch/common/resource/PollutionResource.java
@@ -2,10 +2,8 @@ package greenwatch.common.resource;
 
 import greenwatch.common.vo.PollutionVO;
 
-import java.util.List;
-
 public interface PollutionResource {
-	List<PollutionVO> getPollutions(double lat, double lng);
+	PollutionVO[] getPollutions(double lat, double lng);
 	void storePollution(PollutionVO pollution);
 	void updatePollution(PollutionVO pollution);
 }
