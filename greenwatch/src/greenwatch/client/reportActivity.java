1825be16326331b9a1d6eefacd5f75af32c17ec7
jkabrede@googlemail.com <jkabrede@googlemail.com@24e5345e-f19f-c75b-200d-78b3974f6bd8>
2011-04-16 13:04:33 +0000

    new Activity to handle report.xml

---
+++ greenwatch/src/greenwatch/client/reportActivity.java
@@ -0,0 +1,13 @@
+package greenwatch.client;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class reportActivity  extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.report);
+       
+    }
+
+}
