1f0756259afb08cb52e6f93b70b944d4489e4b7c
Marcus.Schiesser@googlemail.com <Marcus.Schiesser@googlemail.com@24e5345e-f19f-c75b-200d-78b3974f6bd8>
2011-04-16 12:00:42 +0000

    initial release

---
+++ greenwatch-common/src/greenwatch/common/vo/PollutionVO.java
@@ -0,0 +1,67 @@
+package greenwatch.common.vo;
+
+import java.util.Date;
+
+public class PollutionVO {
+	private int id;
+	private Date timestamp;
+	private Long lat;
+	private Long lng;
+	private String imageURL;
+	private enum Intensity { low, high };
+	private Intensity intensity;
+	
+	private String comment;
+	
+	private enum Status { active, inactive };
+	private Status status;
+	
+	public Date getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
+	public Long getLat() {
+		return lat;
+	}
+	public void setLat(Long lat) {
+		this.lat = lat;
+	}
+	public Long getLng() {
+		return lng;
+	}
+	public void setLng(Long lng) {
+		this.lng = lng;
+	}
+	public String getImageURL() {
+		return imageURL;
+	}
+	public void setImageURL(String imageURL) {
+		this.imageURL = imageURL;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	public void setIntensity(Intensity intensity) {
+		this.intensity = intensity;
+	}
+	public Intensity getIntensity() {
+		return intensity;
+	}
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+	public Status getStatus() {
+		return status;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getId() {
+		return id;
+	};
+}
