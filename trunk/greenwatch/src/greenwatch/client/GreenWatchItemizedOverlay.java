9c19e232ae3ebaee96ec31860b5c2922b8d340c5
wernerw.maier@gmail.com <wernerw.maier@gmail.com@24e5345e-f19f-c75b-200d-78b3974f6bd8>
2011-04-16 15:38:31 +0000

    Finished ItemizedOverlay method

--- trunk/greenwatch/src/greenwatch/client/GreenWatchItemizedOverlay.java
+++ trunk/greenwatch/src/greenwatch/client/GreenWatchItemizedOverlay.java
@@ -2,6 +2,8 @@ package greenwatch.client;
 
 import java.util.ArrayList;
 
+import android.app.AlertDialog;
+import android.content.Context;
 import android.graphics.drawable.Drawable;
 
 import com.google.android.maps.ItemizedOverlay;
@@ -10,22 +12,42 @@ import com.google.android.maps.OverlayItem;
 public class GreenWatchItemizedOverlay extends ItemizedOverlay {
 	
 	private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
-
+	Context mContext;
+	
 	public GreenWatchItemizedOverlay(Drawable defaultMarker) {
 		super(defaultMarker);
 		// TODO Auto-generated constructor stub
 	}
+	
+	// to add new overlay items to the ArrayList
+	public void addOverlay(OverlayItem overlay) {
+	    mOverlays.add(overlay);
+	    populate();
+	}
 
 	@Override
 	protected OverlayItem createItem(int i) {
-		// TODO Auto-generated method stub
-		return null;
+		return mOverlays.get(i);
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return mOverlays.size();
 	}
 
+	public GreenWatchItemizedOverlay(Drawable defaultMarker, Context context) {
+		  super(defaultMarker);
+		  mContext = context;
+		}
+	
+	@Override
+	protected boolean onTap(int index) {
+	  OverlayItem item = mOverlays.get(index);
+	  AlertDialog.Builder dialog = new AlertDialog.Builder(mContext);
+	  dialog.setTitle(item.getTitle());
+	  dialog.setMessage(item.getSnippet());
+	  dialog.show();
+	  return true;
+	}
+	
 }
