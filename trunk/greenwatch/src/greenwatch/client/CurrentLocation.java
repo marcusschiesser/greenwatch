096c8c40449ba90926362e319bffb7fd07da7dae
dwaszak@gmail.com <dwaszak@gmail.com@24e5345e-f19f-c75b-200d-78b3974f6bd8>
2011-04-16 13:14:21 +0000

    Facade for location services.

---
+++ trunk/greenwatch/src/greenwatch/client/CurrentLocation.java
@@ -0,0 +1,39 @@
+package greenwatch.client;
+
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+
+/**
+ * Utility class for all location-based services requeired by the application.
+ */
+public class CurrentLocation {
+
+	/**
+	 * Reads the current location of the device using one of the providers (gps,
+	 * network, ip).
+	 * 
+	 * @param locationManager
+	 *            Location manager
+	 * @return Current location of the device
+	 */
+	public static Location getCurrentLocation(LocationManager locationManager) {
+
+		// setup the criteria for finding location using best provider (gps,
+		// ip-based, cell-network based)
+		Criteria criteria = new Criteria();
+		criteria.setAccuracy(Criteria.ACCURACY_FINE);
+		criteria.setAltitudeRequired(false);
+		criteria.setBearingRequired(false);
+		criteria.setCostAllowed(true);
+		criteria.setPowerRequirement(Criteria.POWER_LOW);
+
+		// search for best provider based on the criteria
+		String provider = locationManager.getBestProvider(criteria, true);
+
+		// read the current location using the provider
+		Location location = locationManager.getLastKnownLocation(provider);
+
+		return location;
+	}
+}
