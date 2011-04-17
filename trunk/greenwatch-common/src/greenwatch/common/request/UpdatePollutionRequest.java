18713ebbabfa9dbf35640a6679da38c14ed84bb5
Marcus.Schiesser@googlemail.com <Marcus.Schiesser@googlemail.com@24e5345e-f19f-c75b-200d-78b3974f6bd8>
2011-04-17 12:39:35 +0000

    cleaned up service mock

--- trunk/greenwatch-common/src/greenwatch/common/request/UpdatePollutionRequest.java
+++ trunk/greenwatch-common/src/greenwatch/common/request/UpdatePollutionRequest.java
@@ -1,14 +1,23 @@
 package greenwatch.common.request;
 
+import greenwatch.common.vo.PollutionVO;
+
 public class UpdatePollutionRequest {
 
 	private int id;
+	private PollutionVO pollution;
+
+	public UpdatePollutionRequest(int id, PollutionVO pollution) {
+		super();
+		this.id = id;
+		this.pollution = pollution;
+	}
 
 	public int getId() {
 		return id;
 	}
 
-	public void setId(int id) {
-		this.id = id;
+	public PollutionVO getPollution() {
+		return pollution;
 	}
 }
