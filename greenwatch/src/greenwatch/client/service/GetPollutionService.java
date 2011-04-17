c76bd7c754dd5266407c6a864c161ebd0e363f0a
Marcus.Schiesser@googlemail.com <Marcus.Schiesser@googlemail.com@24e5345e-f19f-c75b-200d-78b3974f6bd8>
2011-04-17 12:39:20 +0000

    cleaned up service mock

--- greenwatch/src/greenwatch/client/service/GetPollutionService.java
+++ greenwatch/src/greenwatch/client/service/GetPollutionService.java
@@ -4,30 +4,15 @@ import greenwatch.common.request.GetPollutionsRequest;
 import greenwatch.common.resource.PollutionResource;
 import greenwatch.common.response.GetPollutionsResponse;
 
-import org.restlet.resource.ClientResource;
 
 import android.os.AsyncTask;
 
 public class GetPollutionService extends AsyncTask<GetPollutionsRequest, Void, GetPollutionsResponse> {
 
-	public static final boolean USE_MOCKS = true;
-
 	@Override
 	protected GetPollutionsResponse doInBackground(GetPollutionsRequest... params) {
-
-		if (USE_MOCKS) { // TODO: use Resources instead
-			try {
-				// sleep to simulate async call
-				Thread.sleep(2000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			return InMemoryStorage.getInstance().getPollutions(params[0]);
-		} else {
-			ClientResource cr = new ClientResource("http://10.0.2.2:8888/rest/pollutions");
-			PollutionResource resource = cr.wrap(PollutionResource.class);
-			return resource.getPollutions(params[0]);
-		}
+		PollutionResource resource = ResourceFactory.createPollutionResource();
+		return resource.getPollutions(params[0]);
 	}
 
 }
